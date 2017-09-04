29ef1e433110b036693a85c29c612f297fe3943b
Grant Iraci <grantira@buffalo.edu>
2017-09-04 16:12:46 -0400

    Inital commit

---
+++ cmake/Modules/si446xConfig.cmake
@@ -0,0 +1,30 @@
+INCLUDE(FindPkgConfig)
+PKG_CHECK_MODULES(PC_SI446X si446x)
+
+FIND_PATH(
+    SI446X_INCLUDE_DIRS
+    NAMES si446x/api.h
+    HINTS $ENV{SI446X_DIR}/include
+        ${PC_SI446X_INCLUDEDIR}
+    PATHS ${CMAKE_INSTALL_PREFIX}/include
+          /usr/local/include
+          /usr/include
+)
+
+FIND_LIBRARY(
+    SI446X_LIBRARIES
+    NAMES gnuradio-si446x
+    HINTS $ENV{SI446X_DIR}/lib
+        ${PC_SI446X_LIBDIR}
+    PATHS ${CMAKE_INSTALL_PREFIX}/lib
+          ${CMAKE_INSTALL_PREFIX}/lib64
+          /usr/local/lib
+          /usr/local/lib64
+          /usr/lib
+          /usr/lib64
+)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SI446X DEFAULT_MSG SI446X_LIBRARIES SI446X_INCLUDE_DIRS)
+MARK_AS_ADVANCED(SI446X_LIBRARIES SI446X_INCLUDE_DIRS)
+
---
+++ python/fix_one_field_packet_handler.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2017 <+YOU OR YOUR COMPANY+>.
+# 
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this software; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+import numpy
+from gnuradio import gr
+
+class fix_one_field_packet_handler(gr.sync_block):
+    """
+    docstring for block fix_one_field_packet_handler
+    """
+    def __init__(self, sync, length, has_crc, check_crc, crc_poly=0x8005):
+        gr.sync_block.__init__(self,
+            name="fix_one_field_packet_handler",
+            in_sig=[<+numpy.float+>],
+            out_sig=None)
+
+
+    def work(self, input_items, output_items):
+        in0 = input_items[0]
+        # <+signal processing here+>
+        return len(input_items[0])
+
