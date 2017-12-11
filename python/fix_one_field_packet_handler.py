8aaa141a2a251b6a4e3abec059fb490d8576a824
Grant Iraci <grantira@buffalo.edu>
2017-12-11 18:30:17 -0500

    Update packet handler block GUI

--- python/fix_one_field_packet_handler.py
+++ python/fix_one_field_packet_handler.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2017 Grant Iraci
+# Copyright 2017 Grant Iraci <grantira@buffalo.edu>
 #
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
